a15350e119c7fdc82728b6f12248da2e3b8850a3
Tim Harford <tim.harford@wgc.school.nz>
2017-10-02 22:08:18 +1300

    Add sessions. Add routes and logic for creating and deleting classes.

--- routes/manageclass.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.get('/manageclass', function(req, res, next) {
-  res.render('manageclass', { title: 'Manage a class' });
-});
-module.exports = router;
